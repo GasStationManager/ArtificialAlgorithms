53caa5d1054b5c0b35f0f34309e13ee146fe8665
GasStationManager <gasstationcodemanager@gmail.com>
2025-07-20 16:42:43 +0000

    initial lean set up

---
+++ lakefile.lean
@@ -0,0 +1,15 @@
+import Lake
+open Lake DSL
+
+package "ArtificialAlgorithms" where
+  -- Settings applied to both builds and interactive editing
+  leanOptions := #[
+    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  ]
+  -- add any additional package configuration options here
+
+require "leanprover-community" / "mathlib" @ git "v4.20.1"
+
+@[default_target]
+lean_lib «ArtificialAlgorithms» where
+  -- add any library configuration options here
