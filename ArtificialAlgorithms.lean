3e812701a53622026d7632a0bdcbcd6638db68ee
GasStationManager <gasstationcodemanager@gmail.com>
2025-07-21 14:51:34 +0000

    knapsack

--- ArtificialAlgorithms.lean
+++ ArtificialAlgorithms.lean
@@ -2,5 +2,6 @@
 -- Import modules here that should be built as part of the library.
 import ArtificialAlgorithms.DynamicProgramming.Memoization
 import ArtificialAlgorithms.DynamicProgramming.RodCutting
+import ArtificialAlgorithms.DynamicProgramming.Knapsack
 import ArtificialAlgorithms.AI.AlphaBeta
 import ArtificialAlgorithms.AI.AlphaBetaClaude
